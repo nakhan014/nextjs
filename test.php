73c44d01c38bdbe061576aa5f76a8b0ff775aadc
nakhan014 <nakhan014@gmail.com>
2025-07-14 12:21:57 +0530

    file added

---
+++ test.php
@@ -0,0 +1,6 @@
+<?php 
+echo "Hello, World!";
+// This is a simple PHP script that outputs a greeting message.
+// You can modify this script to perform other tasks as needed.
+echo "This is a test file for PHP execution.";  
+?>
